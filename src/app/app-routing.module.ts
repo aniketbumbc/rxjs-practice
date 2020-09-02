dbe92cc4bba773521e988e35d53e5d0feda8f81a
Aniket Bhavsar <bhavsar@C02CQ6Q0MD6N.ad.here.com>
2020-09-02 12:30:00 +0530

    feature: promise

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PromiseComponent } from './includes/promise/promise.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'promise', component: PromiseComponent },
+  { path: '**', redirectTo: 'promise' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/includes/promise/promise.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-promise',
+  templateUrl: './promise.component.html',
+  styleUrls: ['./promise.component.scss'],
+})
+export class PromiseComponent implements OnInit {
+  constructor() {}
+  promiseVal:any;
+  ngOnInit(): void {
+    let buyLaptop = new Promise((reslove, reject) => {
+      // reslove("promise reslove yupppee");
+      //reject("Reject Promise")
+
+      if (this.dellLaptop()) {
+        return setTimeout(() => {
+          reslove('Dell is purchased yupppee');
+        }, 3000);
+       
+      } else if (this.hpLaptop()) {
+        return setTimeout(() => {
+          reslove('HP is purchased yupppee');
+        }, 3000);
+      } else {
+        return setTimeout(() => {
+            reject('No laptop is in store');
+        }, 3000);
+      }
+    });
+
+    buyLaptop
+      .then((res) => {
+        console.log('Got it =>', res);
+        this.promiseVal = res;
+      })
+      .catch((e) => {
+        console.log('catch code =>', e);
+        this.promiseVal = e;
+      });
+  }
+
+  dellLaptop() {
+    return false;
+  }
+
+  hpLaptop() {
+    return false;
+  }
+}
